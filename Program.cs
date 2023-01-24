2246289880b9db92234a8cc8d3d009f51e90dd9b
Anna <nusiksuper@gmail.com>
2023-01-24 09:39:21 +0300

    commit 3 Решение задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] GetArray (string [] Array)
+{
+    string [] Result= new string [Array.Length];
+    for (int i=0; i< Array.Length; i++)
+    {
+        if (Array[i].Length<=3)
+        {
+            Result [i]= Array[i];
+        }
+    }
+    return Result;
+}
+
+void PrintArray(string [] inArray)
+{
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        {
+            Console.Write($"{inArray[i]}  ");
+        }
+    }
+}
+
+string [] array= {"1234", "1567", "-2", "computer science"};
+string [] NewArray= GetArray(array);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(NewArray);
